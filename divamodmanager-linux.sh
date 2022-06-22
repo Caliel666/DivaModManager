938342784aecdc11a996d68619cd693d684ed66b
Cali <razeritz420@gmail.com>
2022-06-22 05:39:57 -0300

    not broken

--- divamodmanager-linux.sh
+++ divamodmanager-linux.sh
@@ -56,20 +56,23 @@ command> ";
     mkdir -p "${tempfolder[@]}";
     mkdir -p "${dmm[@]}";
     mkdir -p "${wine[@]}";
-    wget https://github.com/GloriousEggroll/wine-ge-custom/releases/latest/download/wine-lutris-GE-Proton7-16-x86_64.tar.xz -P "${tempfolder[@]}";
-    bsdtar -xJv -f "${tempfolder[@]}/wine-lutris-GE-Proton7-16-x86_64.tar.xz " -C "${wine[@]}"
+    wget https://github.com/GloriousEggroll/wine-ge-custom/releases/latest/download/wine-lutris-GE-Proton7-16-x86_64.tar.xz;
+    tar -xJv -f "./wine-lutris-GE-Proton7-16-x86_64.tar.xz";
+    mv ./lutris-GE-Proton7-16-x86_64/ "${wine[@]}";
     wget https://download.visualstudio.microsoft.com/download/pr/3aa4e942-42cd-4bf5-afe7-fc23bd9c69c5/64da54c8864e473c19a7d3de15790418/windowsdesktop-runtime-5.0.17-win-x64.exe -P "${tempfolder[@]}";
     WINEPREFIX="${wineprefix[@]}" "${wine[@]}/lutris-GE-Proton7-16-x86_64/bin/wine64" "${tempfolder[@]}/"windowsdesktop-runtime-5.0.17-win-x64.exe;
     wget https://github.com/Caliel666/DivaModManager/releases/latest/download/DivaModManager.exe -P "${tempfolder[@]}";
     cp "${tempfolder[@]}/DivaModManager.exe" "${dmm[@]}";
     cp "${wineprefix[@]}/drive_c/Program Files/dotnet/host/fxr/5.0.17/hostfxr.dll" "${dmm[@]}";
     rm -rf "${tempfolder[@]}";
+    rm -rf ./wine-lutris-GE-Proton7-16-x86_64.tar.xz;
+    rm -rf ./wine-lutris-GE-Proton7-16-x86_64.tar.xz.1;
     chmod +x ./run_divamodmanager.sh;
     cp ./run_divamodmanager.sh "${modloader[@]}";
     sed -i "s#prefix#${wineprefix[@]}#g" "${modloader[@]}/run_divamodmanager.sh";
-    sed -i "s#wine#${modloader[@]}/${wine[@]}/lutris-GE-Proton7-16-x86_64/bin/wine64#g" "${modloader[@]}/run_divamodmanager.sh";
-    sed -i "s#DivaModManager.exe#${modloader[@]}/${dmm[@]}/DivaModManager.exe#g" "${modloader[@]}/run_divamodmanager.sh";
-    clear
+    sed -i "s#wine#${wine[@]}/lutris-GE-Proton7-16-x86_64/bin/wine64#g" "${modloader[@]}/run_divamodmanager.sh";
+    sed -i "s#DivaModManager.exe#${dmm[@]}/DivaModManager.exe#g" "${modloader[@]}/run_divamodmanager.sh";
+    clear;
     read -p "Do you wish to add it to your applications menu?
 don't do it if you are on a steam deck
 
@@ -80,21 +83,11 @@ command> ";
         sudo mkdir -p /usr/share/pixmaps/;
         sudo cp ./divamodmanager.desktop /usr/share/applications/;
         sudo cp ./dmmlogo.png /usr/share/pixmaps/;
-        sed -i "s#run_divamodmanager.sh#${modloader[@]}/run_divamodmanager.sh#g" /usr/share/applications/divamodmanager.desktop;
+        sudo sed -i "s#run_divamodmanager.sh#${modloader[@]}/run_divamodmanager.sh#g" /usr/share/applications/divamodmanager.desktop;
         fi
         if [ $REPLY == "no" ]; then
-        clear;
-        ./divamodmanager-linux.sh;
-        fi
-        clear;
-        read -p "All done
-
-enter "q" to go back
-
-command> ";
-        if [ $REPLY == "q" ]; then
-        clear;
-        ./divamodmanager-linux.sh;
+            clear;
+            ./divamodmanager-linux.sh;
         fi
     fi
 
